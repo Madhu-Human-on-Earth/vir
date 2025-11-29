f04303a01e9e043dfff0981f57aa1f630a6daa15
MADHU <116476425+Madhu-Human-on-Earth@users.noreply.github.com>
2025-11-29 22:07:21 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
-# Replace placeholders in config with env values and start turnserver
-envsubst < /etc/turnserver.conf > /etc/turnserver.conf.real
+set -e
+
+# Replace env placeholders in config and start turnserver
+# If TURN_REALM / TURN_USER / TURN_PASS are not set, exit with error
+: "${TURN_REALM:?Environment variable TURN_REALM is required (e.g. randiconnect.com)}"
+: "${TURN_USER:?Environment variable TURN_USER is required}"
+: "${TURN_PASS:?Environment variable TURN_PASS is required}"
+
+# Expand env vars into actual config
+envsubst '\${TURN_REALM} \${TURN_USER} \${TURN_PASS}' < /etc/turnserver.conf > /etc/turnserver.conf.real
+
+# Show some info to logs
+echo "Starting coturn with realm=${TURN_REALM}, user=${TURN_USER}"
+
+# Start coturn in foreground
 exec turnserver -c /etc/turnserver.conf.real --no-daemon
